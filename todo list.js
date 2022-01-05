b711a4e5172444a417c85f42d76741f2334cbb84
Jane Doe <jane.doe@example.com>
2022-01-05 17:55:02 +0330

    create save_local_todos function for add input value in the local storage and , create get_local_todos function for get add value in DOM and refresh values are not deleted , create remove_local_todos function that By removing the value from DOM, the value from local storage is also removed

--- todo list.js
+++ todo list.js
@@ -4,14 +4,14 @@ const todo_input = document.querySelector('.todo-input')
 const todo_button = document.querySelector('.todo-button')
 const todo_list = document.querySelector('.todo-list')
 const filter_option = document.querySelector('.filter-todos')
-
+document.addEventListener('DOMContentLoaded' , get_local_todos)
 
 
 
 
 // event listeners:
 todo_button.addEventListener('click' , add_todo)
-todo_list.addEventListener('click' , check)
+todo_list.addEventListener('click' , check_remove)
 filter_option.addEventListener('click' , filter_todo)
 
 
@@ -30,20 +30,23 @@ function add_todo(e) {
 
     todo_div.innerHTML = new_todo
     todo_list.appendChild(todo_div) 
+    save_local_todos(todo_input.value)
     todo_input.value = ''
 }
 
 
 // trash and chrck-square icone:
-function check(e) {
+function check_remove(e) {
 
-    const check_list = {...e.target.classList}
+    const check_list = [...e.target.classList]
     const item = e.target
     const todo = item.parentElement.parentElement
     
 
     if (check_list[1] === 'fa-trash-alt') {
-        todo.remove()  
+        remove_local_todos(todo)
+        todo.remove() 
+
     }
 
     else if(check_list[1] === 'fa-check-square'){
@@ -89,3 +92,53 @@ function filter_todo (e) {
 
 
 
+
+function save_local_todos(todo) {
+
+    let saved_todos = localStorage.getItem('todos')
+    ? JSON.parse(localStorage.getItem('todos'))
+    :[];
+    
+    saved_todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(saved_todos))
+    
+}
+
+function get_local_todos() {
+
+    let saved_todos = localStorage.getItem('todos')
+    ? JSON.parse(localStorage.getItem('todos'))
+    :[];
+
+    saved_todos.forEach(todo => {
+
+        const todo_div = document.createElement('div')
+        todo_div.className = 'todo'
+        const new_todo = `<li>${todo}</li>
+        <span><i class="far fa-check-square"></i></span>
+        <span><i class="far fa-trash-alt"></i></span>`
+
+        todo_div.innerHTML = new_todo
+        todo_list.appendChild(todo_div) 
+  
+    })
+    
+    
+}
+
+
+function remove_local_todos(todo) {
+
+    // console.log(todo.children[0].innerText)
+
+    let saved_todos = localStorage.getItem('todos')
+    ? JSON.parse(localStorage.getItem('todos'))
+    :[];
+
+    const filtered_todos = saved_todos.filter(
+       (t) => t !== todo.children[0].innerText
+    )
+    
+    localStorage.setItem('todos', JSON.stringify(filtered_todos))
+    
+}
